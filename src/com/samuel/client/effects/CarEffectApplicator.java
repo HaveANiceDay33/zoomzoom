29e338cc0ecdb59e834c6e866897a2b866239bb2
osreboot <calvin.16.weaver@gmail.com>
2018-08-06 13:16:23 -0700

    actually broke the game this time. for real.

--- src/com/samuel/client/effects/CarEffectApplicator.java
+++ src/com/samuel/client/effects/CarEffectApplicator.java
@@ -13,6 +13,7 @@ import com.osreboot.ridhvl.action.HvlAction0;
 import com.osreboot.ridhvl.painter.HvlRenderFrame;
 import com.osreboot.ridhvl.painter.HvlRenderFrame.FBOUnsupportedException;
 import com.osreboot.ridhvl.painter.HvlShader;
+import com.samuel.client.Game;
 import com.samuel.client.MainClient;
 
 public class CarEffectApplicator {
@@ -30,61 +31,63 @@ public class CarEffectApplicator {
 	}
 
 	public static void drawCar(CarEffect effect, float xArg, float yArg, float rotationArg, int carTextureArg, Color color){
-		float timer = MainClient.getNewestInstance().getTimer().getTotalTime();
-		if(effect == null){
-			hvlRotate(xArg, yArg - 30, rotationArg);
-			hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg));
-			hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), color);
-			hvlResetRotation();
-		}else if(effect == CarEffect.DRIFTER){
-			hvlRotate(xArg, yArg - 30, rotationArg);
-			hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg));
-			maskFrame.doCapture(new HvlAction0(){
-				@Override
-				public void run(){
-					hvlRotate(xArg, yArg, -rotationArg + 30);
-					hvlDrawQuadc(xArg, yArg, 2000, 2000, 0f, 0f, 2.0f, 2.0f, MainClient.getTexture(MainClient.DRIFTER_INDEX), color);
-					hvlResetRotation();
-				}
-			});
-			maskShader.doShade(new HvlAction0(){
-				@Override
-				public void run(){
-					maskShader.sendRenderFrame("frame1", 2, maskFrame);
-					hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), Color.white);
-				}
-			});
-			hvlResetRotation();
-		}else if(effect == CarEffect.SHIFTED){
-			hvlRotate(xArg, yArg - 30, rotationArg);
-			hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg));
-			maskFrame.doCapture(new HvlAction0(){
-				@Override
-				public void run(){
-					hvlRotate(xArg, yArg, -rotationArg);
-					Color fadeColor = new Color(color);
-					fadeColor.a = (float)Math.sin(timer)/2f + 0.5f;
-					hvlDrawQuadc(xArg, yArg, 2000f, 2000f, (timer/4f), 0f, (timer/4f) + 2f, 4f, MainClient.getTexture(MainClient.PLASMA1_INDEX), color);
-					hvlDrawQuadc(xArg, yArg, 2000f, 2000f, -(timer/4f), 0f, -(timer/4f) + 2f, 4f, MainClient.getTexture(MainClient.PLASMA2_INDEX), fadeColor);
-					hvlResetRotation();
-				}
-			});
-			hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), color);
-			maskShader.doShade(new HvlAction0(){
-				@Override
-				public void run(){
-					maskShader.sendRenderFrame("frame1", 2, maskFrame);
-					hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), Color.white);
-				}
-			});
-			hvlResetRotation();
-		}else if(effect == CarEffect.NEON){
-			Color neonColor = new Color(1f - color.r, 1f - color.g, 1f - color.b, (3f - (color.r + color.g + color.b))/3f);
-			hvlRotate(xArg, yArg - 30, rotationArg);
-			hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(MainClient.UNDERGLOW_INDEX), neonColor);
-			hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg));
-			hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), color);
-			hvlResetRotation();
+		if(!Game.CAMERA_MODE){
+			float timer = MainClient.getNewestInstance().getTimer().getTotalTime();
+			if(effect == null){
+				hvlRotate(xArg, yArg - 30, rotationArg);
+				hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg));
+				hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), color);
+				hvlResetRotation();
+			}else if(effect == CarEffect.DRIFTER){
+				hvlRotate(xArg, yArg - 30, rotationArg);
+				hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg));
+				maskFrame.doCapture(new HvlAction0(){
+					@Override
+					public void run(){
+						hvlRotate(xArg, yArg, -rotationArg + 30);
+						hvlDrawQuadc(xArg, yArg, 2000, 2000, 0f, 0f, 2.0f, 2.0f, MainClient.getTexture(MainClient.DRIFTER_INDEX), color);
+						hvlResetRotation();
+					}
+				});
+				maskShader.doShade(new HvlAction0(){
+					@Override
+					public void run(){
+						maskShader.sendRenderFrame("frame1", 2, maskFrame);
+						hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), Color.white);
+					}
+				});
+				hvlResetRotation();
+			}else if(effect == CarEffect.SHIFTED){
+				hvlRotate(xArg, yArg - 30, rotationArg);
+				hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg));
+				maskFrame.doCapture(new HvlAction0(){
+					@Override
+					public void run(){
+						hvlRotate(xArg, yArg, -rotationArg);
+						Color fadeColor = new Color(color);
+						fadeColor.a = (float)Math.sin(timer)/2f + 0.5f;
+						hvlDrawQuadc(xArg, yArg, 2000f, 2000f, (timer/4f), 0f, (timer/4f) + 2f, 4f, MainClient.getTexture(MainClient.PLASMA1_INDEX), color);
+						hvlDrawQuadc(xArg, yArg, 2000f, 2000f, -(timer/4f), 0f, -(timer/4f) + 2f, 4f, MainClient.getTexture(MainClient.PLASMA2_INDEX), fadeColor);
+						hvlResetRotation();
+					}
+				});
+				hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), color);
+				maskShader.doShade(new HvlAction0(){
+					@Override
+					public void run(){
+						maskShader.sendRenderFrame("frame1", 2, maskFrame);
+						hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), Color.white);
+					}
+				});
+				hvlResetRotation();
+			}else if(effect == CarEffect.NEON){
+				Color neonColor = new Color(1f - color.r, 1f - color.g, 1f - color.b, (3f - (color.r + color.g + color.b))/3f);
+				hvlRotate(xArg, yArg - 30, rotationArg);
+				hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(MainClient.UNDERGLOW_INDEX), neonColor);
+				hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg));
+				hvlDrawQuadc(xArg, yArg, 100, 100, MainClient.getTexture(carTextureArg + 1), color);
+				hvlResetRotation();
+			}
 		}
 	}
 
